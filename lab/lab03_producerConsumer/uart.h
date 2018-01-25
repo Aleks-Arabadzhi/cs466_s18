18636e60e43ad9b23e6b0d4b8678342a66a79dd3
Miller Lowe <milhead@gmail.com>
2018-01-25 08:11:35 -0800

    changed my mind

--- lab/lab03_producerConsumer/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UART_H_INCLUDED_
-#define _UART_H_INCLUDED_
-
-void uartSetup(void);
-void uartStringPut(const char *s);
-void uartUint32Put(uint32_t value);
-
-#endif //  _UART_H_INCLUDED_
